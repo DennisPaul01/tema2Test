e43d1066a3d7e09a20fd3b13899672c2fcec50be
DennisPaul01 <denys_paul08@yahoo.com>
2023-09-26 14:56:54 +0300

    feat:tema 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,55 @@
-import { Component } from 'react';
-import './App.css';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Component } from "react";
+import "./App.css";
+import { Notify } from "notiflix/build/notiflix-notify-aio";
 
-import ContactForm from './ContactForm';
-import ContactList from './ContactList';
-import Filter from './Filter';
+import ContactForm from "./components/ContactForm";
+import ContactList from "./components/ContactList";
+import Filter from "./components/Filter";
 
 export default class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
-    filter: '',
+    filter: "",
   };
 
   componentDidMount() {
-    document.title = 'HW-2 Phonebook';
+    document.title = "HW-2 Phonebook";
   }
 
   // Add contact
-  handleAddContact = contact => {
+  handleAddContact = (contact) => {
     const { contacts } = this.state;
     const { name } = contact;
 
     // Verify contact
-    if (contacts.find(contact => contact.name === name)) {
+    if (contacts.find((contact) => contact.name === name)) {
       // alert() було використано згідно ДЗ, крок 5.
       // alert(`${name} is already in contacts`);
       Notify.failure(`${name} is already in contacts`);
       return;
     }
 
-    this.setState(prevState => {
+    this.setState((prevState) => {
       return { contacts: [...prevState.contacts, contact] };
     });
   };
 
   // Delete contact
-  handleDeleteContact = id => {
-    this.setState(prevState => {
+  handleDeleteContact = (id) => {
+    this.setState((prevState) => {
       return {
-        contacts: prevState.contacts.filter(contact => contact.id !== id),
+        contacts: prevState.contacts.filter((contact) => contact.id !== id),
       };
     });
   };
 
   // Add filter
-  handleFilter = e => {
+  handleFilter = (e) => {
     this.setState({ filter: e.target.value });
   };
 
